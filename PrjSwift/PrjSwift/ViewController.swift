41d2582d0ae8dab1b9239bbee1d3a603049c2c49
Giang <tgiang81@gmail.com>
2019-08-26 09:25:40 +0700

    First commit

---
+++ PrjSwift/PrjSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrjSwift
+//
+//  Created by Neos on 8/26/19.
+//  Copyright © 2019 Neos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
