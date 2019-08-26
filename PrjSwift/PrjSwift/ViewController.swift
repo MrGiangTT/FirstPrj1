ea8fcfb24c0d4aaae99b132f6b9af8e31ce04c34
Giang <tgiang81@gmail.com>
2019-08-26 11:17:09 +0700

    try to fix error

--- PrjSwift/PrjSwift/ViewController.swift
+++ PrjSwift/PrjSwift/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDiasdfasdfdLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test")
